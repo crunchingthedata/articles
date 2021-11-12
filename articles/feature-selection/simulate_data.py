1f2a9c416bffe2761025a07dcb54bb1d9b31b5cb
Kristen Keller <kristenkeller@Kristens-MBP-2.fios-router.home>
2021-11-12 13:46:42 -0500

    update std multiplier to be on top of mean

--- articles/feature-selection/simulate_data.py
+++ articles/feature-selection/simulate_data.py
@@ -30,7 +30,7 @@ class Simlator:
         self.params = params
         self.n = params.get('global').get('n_observations')
         self.outcome_mean = params.get('variables').get('outcome').get('mean')
-        self.outcome_std = params.get('variables').get('outcome').get('std')
+        self.outcome_std_multiplier = params.get('variables').get('outcome').get('std').get('multiplier')
         self.variable_groups = list(params.get('variables').keys())
         self.numeric_variable_groups = [
             x
@@ -42,7 +42,7 @@ class Simlator:
     def _create_dataframe_with_outcome(self):
         outcome = np.random.normal(
             loc = self.outcome_mean,
-            scale = self.outcome_std,
+            scale = self.outcome_std_multiplier*self.outcome_mean,
             size = self.n
             )
         data = pd.DataFrame(outcome, columns=['outcome'])
@@ -56,10 +56,11 @@ class Simlator:
                 low=group_params['mean_min'],
                 high=group_params['mean_max']
                 )
-            std = np.random.uniform(
-                low=group_params['std_min'],
-                high=group_params['std_max']
+            std_multiplier = np.random.uniform(
+                low=group_params['std_multiplier_max'],
+                high=group_params['std_multiplier_min']
                 )
+            std = mean*std_multiplier
             sample = np.random.normal(mean, std, self.n)
             if group_params['max_order']:
                 order = random.randint(1, group_params['max_order'])
@@ -77,8 +78,8 @@ class Simlator:
             'additive_column': self.params.get('variables').get(group).get('additive_column', None),
             'max_order': self.params.get('variables').get(group).get('max_order', None)
             }
-        group_params['std_min'] = 0
-        group_params['std_max'] = self.outcome_std*group_params['std_multiplier']
+        group_params['std_multiplier_min'] = 0
+        group_params['std_multiplier_max'] = group_params['std_multiplier']
         group_params['mean_min'] = 0
         group_params['mean_max'] = self.outcome_mean*group_params['mean_multiplier']
         return group_params
